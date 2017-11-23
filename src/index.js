5da0d40286fdc03f6f8bcc5488d25749e4bce6a9
Willy Ovalle <willyovalle16@gmail.com>
2017-11-22 20:56:15 -0400

    Feature: initial commit (basic skeleton)

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers/';
+
+import registerServiceWorker from './registerServiceWorker';
+
+import App from './App';
+import './index.css';
+
+const store = createStore(reducers);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+      <App />
+  </Provider>,
+  document.getElementById('root'));
+
+registerServiceWorker();
