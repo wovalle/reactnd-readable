c2b2475185a63e1b833f690c9dcbfbc3527889ea
Willy Ovalle <willyovalle16@gmail.com>
2017-12-14 01:04:17 -0400

    Fix: cleaning comments

--- src/actions/comments.actions.js
+++ src/actions/comments.actions.js
@@ -1,14 +1,2 @@
 import * as actions from '../constants/actions';
 
-export const getAsyncComments = (dispatch, getState) => {
-    return () => {
-        dispatch({
-            type: actions.GET_COMMENTS,
-            comments: [
-                { id: 1, text: 'first' } ,
-                { id: 2, text: 'second' } ,
-                { id: 3, text: 'third' }
-            ]
-        })
-    }
-}
--- src/reducers/comments.reducer.js
+++ src/reducers/comments.reducer.js
@@ -1,12 +1,5 @@
-import * as actions from '../constants/actions';
-
-export default function commentsReducer(state = [], action) {
+export default function commentsReducer(state = {}, action) {
   switch (action.type) {
-    case actions.GET_COMMENTS:
-      return [
-        ...state,
-        ...action.comments,
-      ]
     default:
       return state
   }
