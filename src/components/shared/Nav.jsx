457ba01d01a8d700761252d4d4d4db1cdae856e8
Willy Ovalle <willyovalle16@gmail.com>
2018-01-25 18:09:59 -0400

    Feature: nav logo to redirect to homepage

--- src/components/shared/Nav.jsx
+++ src/components/shared/Nav.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a className="navbar-brand" href="">Readable</a>
+      <Link className="navbar-brand" to="/">Readable</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
